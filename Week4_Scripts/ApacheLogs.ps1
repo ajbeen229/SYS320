43932eb63def35d1384bdc4f89aa6ca03a53bbcb
Alex <alexander.been@mymail.champlain.edu>
2024-09-16 12:38:36 -0400

    Week 4

---
+++ Week4_Scripts/ApacheLogs.ps1
@@ -0,0 +1,10 @@
+﻿clear
+
+function getIPs($page, $code, $browser) {
+
+    $content = Get-Content .\*.log | Select-String $page | Select-String $code | Select-String $browser
+
+    $regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+    $regex.Matches($content) | Select-Object Value
+
+}
---
+++ Week4_Scripts/Week4.ps1
@@ -0,0 +1,41 @@
+﻿clear
+
+#2
+#Get-Content .\access.log
+
+#3
+#Get-Content .\access.log -Tail 5
+
+#4
+#Get-Content .\access.log | Select-String ' 404 ', ' 400 '
+
+#5
+#Get-Content .\access.log | Select-String ' 200 ' -NotMatch
+
+#6
+#Get-ChildItem .\*.log | Select-String 'error'
+
+#7
+<#
+$notfound = Get-Content .\access.log | Select-String ' 404 '
+$regex = [regex] "[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"
+$IPs = $regex.Matches($notfound)
+
+$iplist = @()
+for($i = 0; $i -lt $IPs.Count; $i++) {
+    $iplist += [pscustomobject]@{ "IP" = $IPs[$i].Value; }
+}
+#$iplist | Where-Object {$_.IP -like "10.*"}
+#>
+
+#8
+<#
+$iplist |`
+Where-Object {$_.IP -like "10.*"} |`
+Group IP |`
+Select-Object Count, Name
+#>
+
+#9
+. (Join-Path $PSScriptRoot ApacheLogs.ps1)
+getIPs "index.html" " 404 " "Chrome"
